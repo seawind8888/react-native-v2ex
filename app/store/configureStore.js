3dd7d8cdc62342cdb8adb4af44c0ed70bf3f7a48
seawind8888 <344023858@qq.com>
2017-03-14 18:35:42 +0800

    aaaaa

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -6,6 +6,5 @@ import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer';
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-let store = createStoreWithMiddleware(rootReducer);
+let store = applyMiddleware(thunk)(createStore)(rootReducer);
 export default store;
